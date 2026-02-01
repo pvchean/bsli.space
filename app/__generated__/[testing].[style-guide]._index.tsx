90c938a451a663b6e5da460d53f744e5529fb766
Paul Chean <pvchean@gmail.com>
2026-02-01 16:12:24 -0500

    Auto sync: v20260201.161224

--- app/__generated__/[testing].[style-guide]._index.tsx
+++ app/__generated__/[testing].[style-guide]._index.tsx
@@ -10,7 +10,7 @@ import { Link as Link, Body as Body, RichTextLink as RichTextLink, Link as Link_
 
       export const projectId = "424b8b88-aeeb-4083-bade-6685af7363cb";
 
-      export const lastPublished = "2026-02-01T20:54:11.034Z";
+      export const lastPublished = "2026-02-01T21:11:34.155Z";
 
       export const siteName = undefined;
 
